b35a315341be1ccc8ad83ea1c025b3699dcd7cec
yogeshkumaryadu <yogeshkumaryadu@gmail.com>
2022-10-22 08:43:41 +0530

    Initial commit

---
+++ src/main/java/com/example/GitDemoProject1/GitDemoProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.GitDemoProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoProject1Application.class, args);
+	}
+
+}
